e145af322b564c496a81d14efbdf6ca8ade6087d
biekeb <91184065+biekeb@users.noreply.github.com>
2024-02-19 19:51:31 +0100

    Feat: creating header

---
+++ acterio-fe/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Thoughts</h1>
+      <p>
+        Welcome to Thoughts, where words come alive and ideas flourish! Dive
+        into a world of thought-provoking blog posts that cover a spectrum of
+        topics
+      </p>
+    </div>
+  );
+};
+
+export default Header;
